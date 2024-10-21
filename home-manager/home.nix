f809b58d24af6fdee95fa8421ac6be834f5a7439
Justin Arnold <justin.arnold@programmer.net>
2024-10-20 22:09:19 -0400

    Add neovim
    f
    
    f

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 
 {
+
+  imports =
+    [ 
+      ./nvim.nix
+    ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "justin";
