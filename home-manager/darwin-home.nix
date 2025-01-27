b33a9d048670d5fa73f97d7658d448cac27e26f4
Justin Arnold <justin.arnold@programmer.net>
2025-01-26 23:30:57 -0500

    refactor: Remove go-task

--- home-manager/darwin-home.nix
+++ home-manager/darwin-home.nix
@@ -17,7 +17,6 @@ in {
       ./modules/aerospace.nix
       ./modules/direnv.nix
       ./modules/node.nix
-      ./modules/go-task.nix
       (import ./modules/cglt { inherit paths pkgs secrets config; }) 
     ];
 
