631bc696da1833467836277c1017b2e6b0044749
Justin Arnold <justin.arnold@programmer.net>
2025-05-01 00:40:15 -0400

    chore: Remove old code and update title

--- flake.nix
+++ flake.nix
@@ -1,40 +1,5 @@
-#{
-#  description = "Nix Configuration";
-#
-#  inputs = {
-#    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#    home-manager = {
-#      url = "github:nix-community/home-manager";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-#  };
-#
-#  outputs = { self, nixpkgs, home-manager, ... }:
-#    let
-#      # Determine the target system at runtime
-#      system = "x86_64-linux";
-#      pkgs = nixpkgs.legacyPackages.${system};
-#    in
-#    {
-#      # Define NixOS configurations only for Linux systems
-#      nixosConfigurations = {
-#          thinkpad = nixpkgs.lib.nixosSystem {
-#            inherit system;
-#            modules = [
-#              ./hosts/thinkpad/configuration.nix
-#              home-manager.nixosModules.home-manager
-#              {
-#                home-manager.useGlobalPkgs = true;
-#                home-manager.useUserPackages = true;
-#                home-manager.users.justin = import ./home-manager/justin.nix;
-#             }
-#            ];
-#          };
-#        };      
-#    };
-#}
 {
-  description = "Test Nix Configuration";
+  description = "Personal Nix Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
