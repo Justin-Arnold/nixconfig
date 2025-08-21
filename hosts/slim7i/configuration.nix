426d6d8b0c453f452532865de4815431b4f5ce2e
Justin Arnold <justin.arnold@programmer.net>
2025-08-21 12:21:24 -0400

    feat: Start updating slim7i host

--- hosts/slim7i/configuration.nix
+++ hosts/slim7i/configuration.nix
@@ -8,36 +8,17 @@
       ../../modules/desktop
     ];
 
-     # slim7i = makeNixos [] {
-        #   system = "x86_64-linux";
-        #   modules = [
-        #     ./hosts/slim7i/configuration.nix
-        #     # Add any additional modules here
-        #     home-manager.nixosModules.home-manager
-        #     {
-        #       home-manager.useGlobalPkgs = true;
-        #       home-manager.useUserPackages = true;
-        #       home-manager.backupFileExtension = "backup";
-        #       users.users.justin.home = "/home/justin";
-        #       users.users.justin.isNormalUser = true;
-        #       users.users.justin.group = "justin";
-        #       users.groups.justin = {};
-        #       home-manager.users.justin = import ./home-manager/home.nix;
-        #     }
-        #   ];
-        #   specialArgs = { inherit self; inherit zen-browser;};
-        # };
+    systemProfile = {
+      hostname = "slim7i";
+      stateVersion = "24.05";
+      hasGui = true;
+    };
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
 
   services.xserver = {
     enable = true;
@@ -64,15 +45,9 @@
 
   programs.firefox.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
- 
   services.xserver.videoDrivers = [ "modedriver" ];
   services.xserver.deviceSection = ''
     Option "DRI" "2"
     Option "TearFree" "true"
   '';  
-
-   system.stateVersion = "24.05";
 }
