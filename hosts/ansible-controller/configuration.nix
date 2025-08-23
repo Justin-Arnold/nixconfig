3ba2ac7dd7f55cd26cc3b3641e25b3d34f2ced43
Justin Arnold <justin.arnold@programmer.net>
2025-08-23 18:15:05 -0400

    fix: Fix indentation

--- hosts/ansible-controller/configuration.nix
+++ hosts/ansible-controller/configuration.nix
@@ -20,26 +20,26 @@
   sops.age.keyFile = "/home/justin/.config/sops/age/keys.txt";
 
   # private key -> ~/.ssh/ansible_controller (0400)
-sops.secrets."ssh/ansible_controller/private" = {
-  sopsFile = ../../secrets/ssh.yaml;
-  format   = "yaml";
-  path     = "/home/justin/.ssh/ansible_controller"; #todo use systemProfile.username
-  mode     = "0400";
-  owner    = "justin"; #todo use systemProfile.username
-  group    = "users";
-  neededForUsers = true;
-};
+  sops.secrets."ssh/ansible_controller/private" = {
+    sopsFile = ../../secrets/ssh.yaml;
+    format   = "yaml";
+    path     = "/home/justin/.ssh/ansible_controller"; #todo use systemProfile.username
+    mode     = "0400";
+    owner    = "justin"; #todo use systemProfile.username
+    group    = "users";
+    neededForUsers = true;
+  };
 
-# public key -> ~/.ssh/ansible_controller.pub (0644)
-sops.secrets."ssh/ansible_controller/public" = {
-  sopsFile = ../../secrets/ssh.yaml;
-  format   = "yaml";
-  path     = "/home/justin/.ssh/ansible_controller.pub"; #todo use systemProfile.username
-  mode     = "0644";
-  owner    = "justin"; #todo use systemProfile.username
-  group    = "users";
-  neededForUsers = true;
-};
+  # public key -> ~/.ssh/ansible_controller.pub (0644)
+  sops.secrets."ssh/ansible_controller/public" = {
+    sopsFile = ../../secrets/ssh.yaml;
+    format   = "yaml";
+    path     = "/home/justin/.ssh/ansible_controller.pub"; #todo use systemProfile.username
+    mode     = "0644";
+    owner    = "justin"; #todo use systemProfile.username
+    group    = "users";
+    neededForUsers = true;
+  };
 
   home-manager.users.justin = { ... }: {
     imports = [ 
