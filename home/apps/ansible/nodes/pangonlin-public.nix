e778d36419b8d8b66cac9de0cd9b5629f3eb6d2c
Justin Arnold <justin.arnold@programmer.net>
2025-09-11 01:05:19 -0400

    feat: Add pangolin ansible file

--- home/apps/ansible/nodes/pangonlin-public.nix
+++ home/apps/ansible/nodes/pangonlin-public.nix
@@ -15,53 +15,141 @@ in {
   '';
   #todo centralize the ip and user - also do with terraform steps
   home.file."${proj}/inventory.ini".text = ''
-    [checkmk]
-    10.0.0.68 ansible_user=justin
+    [pangolin]
+    pangolin-public ansible_host=5.161.26.162 ansible_user=justin
   '';
   home.file."${proj}/site.yml".text = ''
     # site.yml
-    - name: Bootstrap NixOS host (Checkmk)
-      hosts: checkmk
-      gather_facts: false
+    - name: Setup Pangolin Public Server
+      hosts: pangolin
+      become: yes
       vars:
-        flake_attr: "checkmk"
-        sops_age_key_src: "~/.config/sops/age/keys.txt"
-        sops_age_key_dest: "/home/justin/.config/sops/age/keys.txt"
+        pangolin_install_dir: /opt/pangolin
+        pangolin_user: pangolin
+        
       tasks:
-        - name: Wait for SSH (using raw)
-          raw: echo "SSH connection ready"
-          register: connection_test
-          until: connection_test is succeeded
-          retries: 30
-          delay: 10
+        - name: Update system packages
+          apt:
+            update_cache: yes
+            upgrade: dist
 
-        - name: Ensure age dir exists with strict perms
-          raw: |
-              sudo -u justin mkdir -p "{{ sops_age_key_dest | dirname }}"
-              sudo chmod 700 "{{ sops_age_key_dest | dirname }}"
+        - name: Install required packages
+          apt:
+            name:
+              - curl
+              - wget
+              - ufw
+              - fail2ban
+              - htop
+              - git
+            state: present
 
-        - name: Copy age private key from controller
-          raw: |
-            # Use base64 to safely transfer the file
-            echo '{{ lookup('file', sops_age_key_src) | b64encode }}' | base64 -d | sudo tee "{{ sops_age_key_dest }}" > /dev/null
-            sudo chown justin:users "{{ sops_age_key_dest }}"
-            sudo chmod 400 "{{ sops_age_key_dest }}"
-          no_log: true
+        - name: Configure firewall rules
+          ufw:
+            rule: allow
+            port: "{{ item.port }}"
+            proto: "{{ item.proto }}"
+          loop:
+            - { port: "22", proto: "tcp" }
+            - { port: "80", proto: "tcp" }
+            - { port: "443", proto: "tcp" }
+            - { port: "51820", proto: "udp" }
+            - { port: "21820", proto: "udp" }
 
-        - name: Switch to your flake (this will install Python and other packages)
-          raw: |
-            sudo nixos-rebuild switch \
-              --flake github:Justin-Arnold/nixconfig#{{ flake_attr }} \
-              --refresh --no-write-lock-file
+        - name: Enable firewall
+          ufw:
+            state: enabled
+            policy: deny
+            direction: incoming
 
-        - name: Gather facts now that system is configured
-          setup:
+        - name: Create pangolin system user
+          user:
+            name: "{{ pangolin_user }}"
+            system: yes
+            shell: /bin/false
+            home: "{{ pangolin_install_dir }}"
+            create_home: no
 
-        - name: Verify system configuration
-          debug:
-            msg: "NixOS system successfully bootstrapped with flake {{ flake_attr }}"
+        - name: Create pangolin directory
+          file:
+            path: "{{ pangolin_install_dir }}"
+            state: directory
+            owner: "{{ pangolin_user }}"
+            group: "{{ pangolin_user }}"
+            mode: '0755'
+
+        - name: Download pangolin installer script
+          get_url:
+            url: https://digpangolin.com/get-installer.sh
+            dest: "{{ pangolin_install_dir }}/get-installer.sh"
+            mode: '0755'
+            owner: "{{ pangolin_user }}"
+            group: "{{ pangolin_user }}"
+
+        - name: Run pangolin installer download
+          shell: |
+            cd {{ pangolin_install_dir }}
+            ./get-installer.sh
+          args:
+            creates: "{{ pangolin_install_dir }}/installer"
+          become_user: "{{ pangolin_user }}"
+
+        - name: Run pangolin installer
+          shell: |
+            cd {{ pangolin_install_dir }}
+            sudo ./installer
+          args:
+            creates: "{{ pangolin_install_dir }}/pangolin"
+
+        - name: Create pangolin systemd service
+          template:
+            src: pangolin.service.j2
+            dest: /etc/systemd/system/pangolin.service
+          notify: restart pangolin
+
+        - name: Enable and start pangolin service
+          systemd:
+            name: pangolin
+            enabled: yes
+            state: started
+            daemon_reload: yes
+
+        - name: Configure fail2ban
+          systemd:
+            name: fail2ban
+            enabled: yes
+            state: started
+
+      handlers:
+        - name: restart pangolin
+          systemd:
+            name: pangolin
+            state: restarted
+            daemon_reload: yes
   '';
+
+  home.file."${proj}/templates/pangolin.service.j2".text = ''
+    [Unit]
+    Description=Pangolin Tunnel Service
+    After=network.target
+    Wants=network.target
+
+    [Service]
+    Type=simple
+    User={{ pangolin_user }}
+    Group={{ pangolin_user }}
+    WorkingDirectory={{ pangolin_install_dir }}
+    ExecStart={{ pangolin_install_dir }}/pangolin
+    Restart=always
+    RestartSec=5
+    StandardOutput=journal
+    StandardError=journal
+
+    [Install]
+    WantedBy=multi-user.target
+  '';
+  
   programs.zsh.shellAliases = {
-    ans-checkmk = "cd ~/${proj} && ansible-playbook site.yml";
+    ans-pangolin = "cd ~/${proj} && ansible-playbook site.yml";
   };
 }
