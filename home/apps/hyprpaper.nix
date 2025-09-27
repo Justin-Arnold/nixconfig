64860ff9b04cf4e4eec46aa8d94502ab3ad98cd5
Justin Arnold <justin.arnold@programmer.net>
2025-09-27 11:58:52 -0400

    feat: Add apps, dotfiles, and roles

---
+++ home/apps/hyprpaper.nix
@@ -0,0 +1,9 @@
+{ pkgs, lib, osConfig, ... }:
+
+{
+  config = lib.mkIf osConfig.systemProfile.hasGui {
+    home.packages = [
+      pkgs.hyprpaper
+    ];
+  };
+}
---
+++ home/roles/nixos.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, osConfig, zen-browser, ... }:
+{
+  imports = [
+    ../apps/krusader.nix
+    ../apps/anyrun.nix
+    ../apps/hyprpaper.nix
+    ../apps/hyprpanel.nix
+  ];
+
+  home.file = {
+    ".config/hypr/hyprland.conf".source = ../dotfiles/hyprland/hyprland.conf;
+    ".config/hypr/hyprpaper.conf".source = ../dotfiles/hyprland/hyprpaper.conf;
+  };
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent ~/.1password/agent.sock
+    '';
+  };
+}
