dca516d8ff6cc79273e2a5f9942dbb5ff1d1d1b5
Justin Arnold <hello@justin-arnold.com>
2025-09-27 22:32:27 -0400

    fix: Update neovim files

--- home/apps/neovim.nix
+++ home/apps/neovim.nix
@@ -5,6 +5,7 @@
 
   home.file = {
     ".config/nvim/init.lua".source = ../dotfiles/nvim/init.lua;
+		".config/nvim/lua/config".source = ../dotfiles/nvim/config;
     ".config/nvim/lua/plugins".source = ../dotfiles/nvim/plugins;
   };
 }
