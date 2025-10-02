bc034b66f0453a4a6f919ab28cd08816ab35656f
Justin Arnold <hello@justin-arnold.com>
2025-10-02 00:54:09 -0400

    feat: Improve oterm

--- home/apps/oterm.nix
+++ home/apps/oterm.nix
@@ -2,8 +2,13 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ oterm ];
-
+  home.packages = [
+    (pkgs.writeShellScriptBin "oterm" ''
+      export OLLAMA_URL="http://10.0.0.63:11434"
+      exec ${pkgs.oterm}/bin/oterm "$@"
+    '')
+  ];
+  home.sessionVariables = { OLLAMA_URL = "http://10.0.0.63:11434"; };
   # Create the popup script
   home.file.".local/bin/oterm-popup" = {
     text = ''
