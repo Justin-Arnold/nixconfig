6aadf70a9cac328065a1c7c381b3ada1c650b573
Justin Arnold <hello@justin-arnold.com>
2026-06-23 04:01:18 -0400

    Wrap GitKraken for desktop scaling

--- home/apps/gitkraken.nix
+++ home/apps/gitkraken.nix
@@ -1,9 +1,36 @@
 { pkgs, lib, osConfig, ... }:
 
-{
+# GitKraken has an issue with honoring GUI scaling in linux
+# so this file is a little more cumbersome to ensure we pass
+# the required flag to the program to make sure it matches our
+# desired UI scaling
+
+let
+  gitkrakenScaled = pkgs.symlinkJoin {
+    name = "gitkraken-scaled";
+    paths = [ pkgs.gitkraken ];
+
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    postBuild = ''
+      wrapProgram $out/bin/gitkraken \
+        --add-flags "--force-device-scale-factor=1.25"
+    '';
+  };
+in {
   config = lib.mkIf osConfig.systemProfile.hasGui {
     home.packages = [
-      # pkgs.gitkraken
+      gitkrakenScaled
     ];
+
+    # This makes sure the app launchers using the wrapped binary
+    xdg.desktopEntries.gitkraken = {
+      name = "GitKraken";
+      genericName = "Git Client";
+      exec = "gitkraken %U";
+      icon = "gitkraken";
+      terminal = false;
+      categories = [ "Development" ];
+    };
   };
 }
